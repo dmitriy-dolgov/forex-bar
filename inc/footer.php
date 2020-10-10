e08c25b1ab9fb5376c73e649822cdbc359509f49
dmitriy-dolgov <TwilightTower@mail.ru>
2020-10-10 14:05:25 +0300

    footer added

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+    </div>
+</footer>
 <?php if (IS_INTERNET): ?>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
--- inc/header.php
+++ inc/header.php
@@ -7,29 +7,30 @@ include 'config.php';
     <meta charset="UTF-8">
     <title>FOREX BAR</title>
     <?php if (IS_INTERNET): ?>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+              integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+              crossorigin="anonymous">
     <?php else: ?>
-        <link rel="stylesheet" href="inc/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
     <?php endif; ?>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
-<nav class="navbar navbar-expand-md navbar-dark bg-primary">
-    <div class="d-flex w-50">
-        <a class="navbar-brand mr-0" href="#">FOREX BAR</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-    </div>
-    <div class="navbar-collapse collapse justify-content-center" id="collapsingNavbar">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">BIDLeveL</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Контакты</a>
-            </li>
-        </ul>
-    </div>
-    <span class="navbar-text small text-truncate mt-1 w-50 text-right">Присоединиться</span>
-</nav>
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+        <div class="d-flex w-50">
+            <a class="navbar-brand mr-0" href="#">FOREX <> BAR</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="navbar-collapse collapse justify-content-center" id="collapsingNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">BIDLeveL</a>
+                </li>
+            </ul>
+        </div>
+        <span class="navbar-text small text-truncate mt-1 w-50 text-right">Присоединиться</span>
+    </nav>
+</header>
